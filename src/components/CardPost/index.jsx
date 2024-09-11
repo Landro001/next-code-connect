1e480f838ba40b0ccc0df9524b9b84d903bb26f4
Landro <ljborges76@gmail.com>
2024-09-11 16:28:55 -0300

    Páginas dinâmicas

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,28 +1,30 @@
 import Image from "next/image";
 import { Avatar } from "../Avatar";
-import styles from './cardpost.module.css'
+import styles from "./cardpost.module.css";
+import Link from "next/link";
 
-export const CardPost = ({ post }) => {
+export const CardPost = ({ post, highlight }) => {
   return (
-    <article className={styles.card}>
-      <header className={styles.banner}>
-        <figure>
-          <Image
-            src={post.cover}
-            width={438}
-            height={133}
-            alt={`Capa do post de título: ${post.title}`}
-            className={styles.image}
-          />
-        </figure>
-      </header>
-      <section className={styles.content}>
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
-      </section>
-      <footer className={styles.signature}>
-        <Avatar imageSrc={post.author.avatar} name={post.author.username} />
-      </footer>
-    </article>
+    <Link href={`/posts/${post.slug}`} className={styles.link}>
+      <article className={styles.card} style={{ width: highlight ? 993 : 486 }}>
+        <header className={styles.banner}>
+          <figure style={{ height: highlight ? 300 : 133 }}>
+            <Image
+              src={post.cover}
+              fill             
+              alt={`Capa do post de título: ${post.title}`}
+              className={styles.image}
+            />
+          </figure>
+        </header>
+        <section className={styles.content}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </section>
+        <footer className={styles.signature}>
+          <Avatar imageSrc={post.author.avatar} name={post.author.username} />
+        </footer>
+      </article>
+    </Link>
   );
 };
